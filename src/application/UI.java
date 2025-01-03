b50a9b6cf43297d66764c0f6959d3b59d1f0993b
Caio <caio_25m@hotmail.com>
2025-01-02 21:58:32 -0300

    Comentarios

--- src/application/UI.java
+++ src/application/UI.java
@@ -60,6 +60,7 @@ public class UI {
 		System.out.println("  a b c d e f h");
 	}
 	
+	//aqui esta imprimindo as possiveis posições da peça de origem
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
