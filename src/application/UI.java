bad2f474012ded28d1d93d9c0dad21426d227a96
Caio <caio_25m@hotmail.com>
2025-01-16 11:05:36 -0300

    Check Mate Logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -37,7 +37,8 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
-	//esse metodo esta fazendo ao limpeza do tabueliro para não ficar puluido no terminal
+	// esse metodo esta fazendo ao limpeza do tabueliro para não ficar puluido no
+	// terminal
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
@@ -53,18 +54,23 @@ public class UI {
 			throw new InputMismatchException("Error reading ChessPosition. valid values are from a1 to h8. ");
 		}
 	}
-	
+
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting player " + chessMatch.getCurrentplayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK! ");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player " + chessMatch.getCurrentplayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("YOU ARE IN CHECK! ");
+			}
 		}
-		
+		else {
+			System.out.println("CHECKMATE! ");
+			System.out.println("Waiting Player: " + chessMatch.getCurrentplayer());
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -77,8 +83,8 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
-	//aqui esta imprimindo as possiveis posições da peça de origem
+
+	// aqui esta imprimindo as possiveis posições da peça de origem
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -107,20 +113,21 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
-	private static void printCapturedPieces(List<ChessPiece> captured) {//metodo de captura de peças
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+
+	private static void printCapturedPieces(List<ChessPiece> captured) {// metodo de captura de peças
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+				.collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+				.collect(Collectors.toList());
 		System.out.println("Captured pieces: ");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
-		System.out.println(Arrays.toString(white.toArray()));//esta imprimindo array de valores no java
+		System.out.println(Arrays.toString(white.toArray()));// esta imprimindo array de valores no java
 		System.out.println(ANSI_RESET);
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
-		System.out.println(Arrays.toString(black.toArray()));//esta imprimindo array de valores no java
+		System.out.println(Arrays.toString(black.toArray()));// esta imprimindo array de valores no java
 		System.out.println(ANSI_RESET);
-		
-		
+
 	}
 }
